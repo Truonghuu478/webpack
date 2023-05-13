aaa91c3964cd1f0b6941b1bd2e9d298210f41b4d
truongle1812 <david.le@lixibox.com>
2023-05-13 19:19:22 +0700

    study label and bundle analys

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import { sum } from "./utils";
+import { reject } from "lodash";
 
 console.log(
     'result',
     sum(2,3)
 );
 
+
+console.log(
+   Object.values({"name":"truong","name":"vy"})
+);
+
+import cv from "./pdfs/Le-Huu-Truong_CV.pdf?page=1"
+
+
+
+const domHandler = () => {
+    const link = document.createElement('a')
+    link.href = cv
+    link.textContent = 'CV Trường'
+    document.body.appendChild(link)
+  }
+const handle = ()=>
+    new Promise((resolve)=>
+        setTimeout(()=>{
+            resolve(100)
+        },1000)
+    )
+
+const main = async ()=>{
+    const result = await handle();
+    console.log('result',result);
+}
+main()
+
+domHandler()
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // tạo ra 1 file html từ config webpack
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // tách ra một file css riêng
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-module.exports = () => {
+module.exports = (env) => {
   // const isDev = Boolean(env.development)
   const envDevelopment = "development";
   const isDev = process.env.NODE_ENV === envDevelopment;
+  const basePlugin = [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name][contenthash].css",
+    }),
+  ];
   return {
     mode: isDev === isDev ? envDevelopment : "production",
     entry: {
@@ -14,7 +25,8 @@ module.exports = () => {
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "[name][contenthash].js",
-      clean: true, // doạn dẹp file cũ sau mỗi lần build
+      clean: true, // đoạn clear file cũ sau mỗi lần build
+      assetModuleFilename: "[file][query]",
     },
     devtool: isDev === isDev ? "source-map" : false,
     module: {
@@ -29,28 +41,32 @@ module.exports = () => {
           use: {
             loader: "babel-loader",
             options: {
-              presets: [["@babel/preset-env", { targets: "defaults" }]],
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    debug: true, //Hiển thị debug lên terminal để dễ debug
+                    useBuiltIns: "usage", //Dùng cái này thì đơn giản nhất vì không cần import core-js vào code
+                    corejs: "3.30.2", //nên quy định version core-js để babel-preset-env có hoạt động tối ưu
+                  },
+                ],
+              ],
             },
           },
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+          type: "asset/resource",
+        },
       ],
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "Webpack App",
-        filename: "index.html",
-        template: "src/template.html",
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name][contenthash].css",
-      }),
-    ],
+    plugins: isDev ? [...basePlugin, new BundleAnalyzerPlugin()] : basePlugin,
     devServer: {
       static: {
         directory: "dist", // đường dẫn tương đối đến với thư mục chứa index.html
       },
-      port: 4200, //Pỏt thay cho poẻ mặc định (8080)
-      open: true, //mở trnag webpack khi chạy terminal
+      port: 4200, //Port thay cho port mặc định (8080)
+      open: true, //mở trang webpack khi chạy terminal
       hot: true, //Bật tính năng reload nhanh Hot Module Replacement
       compress: true, // Bật Gzip cho các tài nguyên
       historyApiFallback: true, //Set true nếu bạn dùng cho ác SPA và sử dụng History API của HTML5
